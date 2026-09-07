b31ec18742a59233870ab5a8408464aa316c3d1b
swarna38 <swarna.sutradhar.info@gamil.com>
2026-09-07 15:20:50 +0600

    nvbar done and

---
+++ src/Navbar.tsx
@@ -0,0 +1,47 @@
+import { FaCartPlus } from "react-icons/fa";
+import { IoSearchSharp } from "react-icons/io5";
+import logo from './assets/img/nav-logo.png';
+
+const Navbar = () => {
+    return (
+        <div className="border-b border-gray-300">
+            <nav className="flex justify-between gap-4 container mx-auto py-4 ">
+                <img src={logo} className="w-[120px] h-[60px]" alt="" />
+                <ul className="flex gap-5 items-center text-xl font-semibold">
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/">About Us</a>
+                </li>
+                <li>
+                    <a href="/">Contact</a>
+                </li>
+                <li>
+                    <a href="/">Services</a>
+                </li>
+                </ul>
+
+                <div className="flex gap-4 items-center">
+                <IoSearchSharp />
+
+                <FaCartPlus />
+
+
+                <span className="bg-[#179800] text-white px-2 py-1 rounded-full text-sm">
+                    
+                </span>
+
+                <button className="bg-blue-500 text-white px-6 text-xl  py-2 rounded-md hover:bg-blue-600 hover:text-orange-500 hover:scale-110 transition-transform duration-300">
+                    Login
+                </button>
+                <button className="bg-green-500 text-white px-6 text-xl  py-2 rounded-md hover:bg-green-600">
+                    Signup
+                </button>
+                </div>
+            </nav>
+    </div>
+    );
+};
+
+export default Navbar;
